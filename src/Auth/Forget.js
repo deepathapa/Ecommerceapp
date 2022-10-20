a67a6c3351e5378c791175faee0547ca49a2e9ab
pankaj1920 <bohrapankaj1920@gmail.com>
2022-10-20 07:31:50 +0530

    Fix

--- src/Auth/Forget.js
+++ src/Auth/Forget.js
@@ -1,85 +1,78 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
-import axios from "../Axios/Axios";
-import Otp from "./Otp";
-
+import { useNavigate } from 'react-router-dom';
+import axios from '../Axios/Axios';
+import Otp from './Otp';
 
 const Forget = () => {
-const navigate = useNavigate();
-  const [forgetdata, SetForgetdata] = useState(
-   
-    {
-      username: ''
-    }
-  );
-  //console.log("number",forgetdata);
-  console.log("mobiledatanumber",forgetdata)
+  const navigate = useNavigate();
+
+  const [userName, setUserName] = useState();
+
   const handleforget = (e) => {
-    SetForgetdata({ ...forgetdata, [e.target.name]: e.target.value });
-  }
-   
-  const[formerror,setFormerror] = useState({})
+    setUserName(e.target.value);
+  };
+
+  const [formerror, setFormerror] = useState({});
 
   const subimtForget = async (event) => {
     event.preventDefault();
-    setFormerror(validationform(forgetdata));
     const sendOtp = {
-      username: forgetdata.username,
+      username: userName,
+    };
+    const result = await axios.post('/generateOtp', sendOtp);
+    console.log('mobilenumber', sendOtp);
+    if (result.data.statuscode === 'TXN') {
+      navigate('/Otp', {
+        state: { otp: result.data.data, username: userName },
+      });
+    } else {
+      alert('User not found');
     }
-   const result = await axios.post('/generateOtp', sendOtp)
-   console.log("mobilenumber",sendOtp);
-   if(result.data.statuscode === 'TXN'){
- 
-  navigate('/Otp',{state:{id:result.data.data}},{state:{mobiledata:sendOtp}});
-  }else {
-    alert("User not found");
-  }
-     
-  }
+  };
 
   // validation function
   const validationform = (value) => {
     const errors = {};
     const userPattern = /^[0-9]{10}$/;
-    if(!value.username){
-      errors.username="please enter mobile number"
-    }else if(!userPattern.test(value.username)){
-      errors.username="invalid mobile number"
+    if (!value.username) {
+      errors.username = 'please enter mobile number';
+    } else if (!userPattern.test(value.username)) {
+      errors.username = 'invalid mobile number';
     }
     return errors;
-  }
+  };
   return (
     <>
-     <section className="bg_wrapper">
-     <div className="container">
-        <form onSubmit={subimtForget} >
-          <div className="form first">
-            <div className="details personal">
-              <div className="field_grid">
-                <div className="input_field">
-                
-                  <label>Mobile Number</label>
-                  <input type="text" placeholder='Enter mobile' value={forgetdata.username} name="username" onChange={handleforget} autoComplete="off"/>
-                  <span className="error">{formerror.username}</span>
+      <section className="bg_wrapper">
+        <div className="container">
+          <form onSubmit={subimtForget}>
+            <div className="form first">
+              <div className="details personal">
+                <div className="field_grid">
+                  <div className="input_field">
+                    <label>Mobile Number</label>
+                    <input
+                      type="text"
+                      placeholder="Enter mobile"
+                      value={userName}
+                      name="username"
+                      onChange={handleforget}
+                      autoComplete="off"
+                    />
+                    <span className="error">{formerror.username}</span>
+                  </div>
+
+                  <button type="submit" className="sub_btn">
+                    GET OTP
+                  </button>
                 </div>
-                
-               
-                <button type="submit" className="sub_btn">GET OTP</button>
               </div>
             </div>
-
-          </div>
-        </form>
-        <Otp />
-        
-        
-
-      </div>
-     </section>
-     
-      
+          </form>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Forget
+export default Forget;
--- src/Auth/Otp.js
+++ src/Auth/Otp.js
@@ -2,54 +2,54 @@ import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 
-const Otp = ({mobiledata}) => {
- console.log("deepa",mobiledata)
+const Otp = ({ mobiledata }) => {
   const navigate = useNavigate();
   const location = useLocation();
-  console.log(location, "useHook location")
-  const data = location.state?.id;
-  const numberdata = location.state?.mobiledata;
-  console.log("USERMOBILE",numberdata);
- 
+  const userData = location.state;
+  const otp = userData?.otp;
+  const userName = userData?.username;
+  console.log(`USERMOBILE => ${userName}  OTP => ${otp}`);
 
-  
-  
   const updatePassword = (e) => {
-     e.preventDefault();
-     if(data){
+    e.preventDefault();
+    /*   if(data){
       navigate('/updateOtp',{state:{id:data}})
     }
-     
-  }
+      */
+  };
   return (
     <>
-     <section className="bg_wrapper">
-     <div className="container">
-        <form onSubmit={updatePassword} >
-          <div className="form first">
-            <div className="details personal">
-              <div className="field_grid">
-                <div className="input_field">
-               <div></div>
-                  <label>OTP</label>
-                  
-                  <input type="text" placeholder='Enter mobile' defaultValue={data} name="username" autoComplete="off" />
-                  <span className="error"></span>
-                </div>
+      <section className="bg_wrapper">
+        <div className="container">
+          <form onSubmit={updatePassword}>
+            <div className="form first">
+              <div className="details personal">
+                <div className="field_grid">
+                  <div className="input_field">
+                    <div></div>
+                    <label>OTP</label>
 
-               
-                <button type="submit" className="sub_btn">update  OTP</button>
+                    <input
+                      type="text"
+                      placeholder="Enter mobile"
+                      defaultValue={''}
+                      name="username"
+                      autoComplete="off"
+                    />
+                    <span className="error"></span>
+                  </div>
+
+                  <button type="submit" className="sub_btn">
+                    update OTP
+                  </button>
+                </div>
               </div>
             </div>
-
-          </div>
-        </form>
-        
-      </div>
-     </section>
-      
+          </form>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
 export default Otp
